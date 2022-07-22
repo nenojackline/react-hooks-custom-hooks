888f3bd0d37d3b7e86cf0502129195266eb22801
jackline chepngeno <chepngenojackline0@gmail.com>
2022-07-22 10:56:35 +0300

    commit tree 997299dbf222ae16a895741e9ffa1c8bd76ca2e6

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -1,26 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import useDocumentTitle from "../hooks/useDocumentTitle";
 import { makeEmojiList } from "../utils";
 
 function ArticlePage() {
-  // fetch data for a post
   const { id } = useParams();
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [post, setPost] = useState(null);
-
-  const url = `http://localhost:4000/posts/${id}`;
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch(url)
-      .then((r) => r.json())
-      .then((post) => {
-        setPost(post);
-        setIsLoaded(true);
-      });
-  }, [url]);
+  const { data: post, isLoaded } = useQuery(
+    `http://localhost:3001/posts/${id}`
+  );
 
   // set the document title
   const pageTitle = post ? `Underreacted | ${post.title}` : "Underreacted";
+
+  useDocumentTitle(pageTitle)
   useEffect(() => {
     document.title = pageTitle;
   }, [pageTitle]);
